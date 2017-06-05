71596649fe03f0ddad773d7cd4c291afdae40d69
Dilocker Etienne <etienne.dilocker@metrosystems.net>
2017-06-05 17:15:52 +0200

    improve manager and add createPlan

--- manager/services/manager.js
+++ manager/services/manager.js
@@ -1,8 +1,12 @@
+const shouldWaterNow = (now, start, end) => {
+  return now >= start && now <= end
+}
+
 const manager = (now, plan) => {
   return plan.reduce( (accu, curr) => {
     return Object.assign(
       accu,
-      {[curr.zone] : {water: false}}
+      {[curr.zone] : {water: shouldWaterNow(now, curr.start, curr.end)}}
     )
   }, {})
 }
